4479ef25d822c664fae2a85ef6e500ec3e451661
Dancovich <danilo.viana@serpro.gov.br>
2013-01-17 19:44:14 -0200

    Versão inicial de testes da extensão Demoiselle JMX

---
+++ incubator/jmx/src/test/java/br/gov/frameworkdemoiselle/jmx/dao/PersistedTestClassDAO.java
@@ -0,0 +1,11 @@
+package br.gov.frameworkdemoiselle.jmx.dao;
+
+import br.gov.frameworkdemoiselle.jmx.domain.PersistedTestClass;
+import br.gov.frameworkdemoiselle.template.JPACrud;
+
+
+public class PersistedTestClassDAO extends JPACrud<PersistedTestClass, Long> {
+
+	private static final long serialVersionUID = 1L;
+
+}
