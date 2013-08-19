e79b0f83c24395f30944dd81d958f9373d17bfbb
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2013-08-19 08:25:21 -0300

    Criação do projeto com base no arquétipo JSF-JPA

--- experiments/arquillian/src/test/java/exp/business/BookmarkBCTest.java
+++ experiments/arquillian/src/test/java/exp/business/BookmarkBCTest.java
@@ -1,4 +1,4 @@
-package br.gov.serpro.frameworkdemoiselle.provaconceito.business;
+package exp.business;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -7,20 +7,19 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import br.gov.serpro.frameworkdemoiselle.arquillian.DemoiselleArquillian;
-import br.gov.serpro.frameworkdemoiselle.provaconceito.domain.Bookmark;
+import br.gov.frameworkdemoiselle.junit.DemoiselleRunner;
+import exp.domain.Bookmark;
 
-@RunWith(Arquillian.class)
-public class BookmarkBCArquillianTest extends DemoiselleArquillian {
+@RunWith(DemoiselleRunner.class)
+public class BookmarkBCTest {
 
 	@Inject
 	private BookmarkBC bookmarkBC;
-
+	
 	@Before
 	public void before() {
 		for (Bookmark bookmark : bookmarkBC.findAll()) {
@@ -35,7 +34,7 @@ public class BookmarkBCArquillianTest extends DemoiselleArquillian {
 		assertNotNull(listaBookmarks);
 		assertEquals(10, listaBookmarks.size());
 	}
-
+	
 	@Test
 	public void testInsert() {
 		Bookmark bookmark = new Bookmark("Demoiselle Portal", "http://www.frameworkdemoiselle.gov.br");
@@ -44,37 +43,35 @@ public class BookmarkBCArquillianTest extends DemoiselleArquillian {
 		assertNotNull(listaBookmarks);
 		assertEquals(1, listaBookmarks.size());
 	}
-
+	
 	@Test
 	public void testDelete() {
 		Bookmark bookmark = new Bookmark("Demoiselle Portal", "http://www.frameworkdemoiselle.gov.br");
 		bookmarkBC.insert(bookmark);
-
+		
 		List<Bookmark> listaBookmarks = bookmarkBC.findAll();
 		assertNotNull(listaBookmarks);
 		assertEquals(1, listaBookmarks.size());
-
+		
 		bookmarkBC.delete(bookmark.getId());
 		listaBookmarks = bookmarkBC.findAll();
 		assertEquals(0, listaBookmarks.size());
 	}
-
 	@Test
 	public void testUpdate() {
 		Bookmark bookmark = new Bookmark("Demoiselle Portal", "http://www.frameworkdemoiselle.gov.br");
 		bookmarkBC.insert(bookmark);
-
+		
 		List<Bookmark> listaBookmarks = bookmarkBC.findAll();
-		Bookmark bookmark2 = (Bookmark) listaBookmarks.get(0);
+		Bookmark bookmark2 = (Bookmark)listaBookmarks.get(0);
 		assertNotNull(listaBookmarks);
 		assertEquals("Demoiselle Portal", bookmark2.getDescription());
-
+		
 		bookmark2.setDescription("Demoiselle Portal alterado");
 		bookmarkBC.update(bookmark2);
-
+		
 		listaBookmarks = bookmarkBC.findAll();
-		Bookmark bookmark3 = (Bookmark) listaBookmarks.get(0);
+		Bookmark bookmark3 = (Bookmark)listaBookmarks.get(0);
 		assertEquals("Demoiselle Portal alterado", bookmark3.getDescription());
 	}
-
 }
