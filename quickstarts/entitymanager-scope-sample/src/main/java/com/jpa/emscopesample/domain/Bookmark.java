008c6bbe6cf33a0fc35afde7bf8a40106cdc4d32
Dancovich <danilo.viana@serpro.gov.br>
2013-10-02 17:08:02 -0300

    IN PROGRESS - issue FWK-135: Postar sobre escopo selecionável do EntityManager https://demoiselle.atlassian.net/browse/FWK-135

---
+++ quickstarts/entitymanager-scope-sample/src/main/java/com/jpa/emscopesample/domain/Bookmark.java
@@ -0,0 +1,78 @@
+package com.jpa.emscopesample.domain;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Bookmark implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 *  If you are using Glassfish then remove the strategy attribute
+	 */
+	@Id
+	@GeneratedValue(strategy = SEQUENCE)
+	private Long id;
+	
+	@Column
+	private String description;
+	
+	@Column
+	private String link;
+	
+	@OneToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="id_bookmark" , referencedColumnName="id")
+	private List<Category> categories;
+	
+	public Bookmark() {
+		super();
+	}
+	
+	public Bookmark(String description, String link) {
+		this.description = description;
+		this.link = link;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getLink() {
+		return link;
+	}
+	
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+}
---
+++ quickstarts/entitymanager-scope-sample/src/main/java/com/jpa/emscopesample/persistence/BookmarkDAO.java
@@ -0,0 +1,13 @@
+package com.jpa.emscopesample.persistence;
+
+import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
+import br.gov.frameworkdemoiselle.template.JPACrud;
+
+import com.jpa.emscopesample.domain.Bookmark;
+
+@PersistenceController
+public class BookmarkDAO extends JPACrud<Bookmark, Long> {
+	
+	private static final long serialVersionUID = 1L;
+	
+}
