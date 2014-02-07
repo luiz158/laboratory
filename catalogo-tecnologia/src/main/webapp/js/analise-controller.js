34ab5cd1619c2f5c96ddfa33c4b7dbb67f84bffc
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2014-02-07 09:11:48 -0300

    Refatoração para simplificação da função Salvar.

--- catalogo-tecnologia/src/main/webapp/js/analise-controller.js
+++ catalogo-tecnologia/src/main/webapp/js/analise-controller.js
@@ -18,51 +18,28 @@ controllers.controller('Analise', function Analise($scope, $http) {
 
 	$scope.salvar = function() {
 		console.log("AnaliseController " + $scope.analise);
-		if ($scope.analise.id)
-			$http({
-				url : 'api/analise',
-				method : "PUT",
-				data : $scope.analise,
-				headers : {
-					'Content-Type' : 'application/json;charset=utf8'
-				}
 
-			}).success(function(data) {
-				$("[id$='-message']").text("");
-				carregarAnalises();
+		$http({
+			url : 'api/analise',
+			method : $scope.analise.id ? "PUT" : "POST",
+			data : $scope.analise,
+			headers : {
+				'Content-Type' : 'application/json;charset=utf8'
+			}
 
-			}).error(
-					function(data, status) {
-						if (status = 412) {
-							$.each(data, function(i, violation) {
-								$("#" + violation.property + "-message").text(
-										violation.message);
-							});
-						}
-					});
-		else {
-			$http({
-				url : 'api/analise',
-				method : "POST",
-				data : $scope.analise,
-				headers : {
-					'Content-Type' : 'application/json;charset=utf8'
-				}
+		}).success(function(data) {
+			$("[id$='-message']").text("");
+			carregarAnalises();
 
-			}).success(function(data) {
-				$("[id$='-message']").text("");
-				carregarAnalises();
-
-			}).error(
-					function(data, status) {
-						if (status = 412) {
-							$.each(data, function(i, violation) {
-								$("#" + violation.property + "-message").text(
-										violation.message);
-							});
-						}
-					});
-		}
+		}).error(
+				function(data, status) {
+					if (status = 412) {
+						$.each(data, function(i, violation) {
+							$("#" + violation.property + "-message").text(
+									violation.message);
+						});
+					}
+				});
 	};
 
 	$scope.editar = function(analise) {
