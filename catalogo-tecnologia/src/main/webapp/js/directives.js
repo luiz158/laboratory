c5389dfb341ffefd2e44e3c545982a4969f6163f
Robson Ximenes <robsonximenes@gmail.com>
2014-02-07 17:40:38 -0300

    Refatorada a diretiva.

--- catalogo-tecnologia/src/main/webapp/js/directives.js
+++ catalogo-tecnologia/src/main/webapp/js/directives.js
@@ -1,43 +1,32 @@
 
 var diretivas = angular.module('catalogo.directives', []);
 
-diretivas.directive('ngSparkline', function() {
-  return {
-    restrict: 'A',
-    require: '^ngCity',
-    scope: {
-      ngCity: '@'
-    },
-    template: '<div class="sparkline"><h4>Weather for {{ngCity}}</h4></div>',
-    link: function(scope, iElement, iAttrs) {
-      // get weather details
-    }
-  }
-});
-
-
 diretivas.directive('ngAnaliseSituacao', function() {
 
 	  return {
-		//restrict: 'E',
+		//restrict: 'C',
 	    //require: '^ngSituacao',
 	    scope: {
-	      ngSituacao: '@'
-	    },		  
-	    template: '<a ng-click="editar(a)" class="btn"><i class="fa"> </i> {{ngSituacao}}</a>',
+	      situacao: '@'
+	    },	
+	    template: '<span class="label"><i class="fa"> </i> {{situacao}}</span>',
 	    
-	    link: function(scope, iElement, iAttrs) {
-	    	if (scope.ngSituacao == 'Aprovado'){
-	    		$(iElement).children().first().addClass('btn-success');
-		    	$(iElement).children().first().children().first().addClass('fa-thumbs-o-up');
-	    	} else 
-	    	if (scope.ngSituacao == 'Reprovado') {
-	    		$(iElement).children().first().addClass('btn-danger');
-		    	$(iElement).children().first().children().first().addClass('fa-thumbs-o-down');
-	    	} else {
-	    		$(iElement).children().first().addClass('btn-primary');
-		    	$(iElement).children().first().children().first().addClass('fa-pencil-square-o');
-	    	}
+	    link: function(scope, elem, $attrs) {
+	    	
+	    	$attrs.$observe('situacao', function(situacao) {
+	    		var labelType = 'label-primary';
+	    		var icon = 'fa-pencil-square-o';
+		    	if (situacao == 'Aprovado'){
+		    		var labelType = 'label-success';
+		    		var icon = 'fa-thumbs-o-up';
+		    	} else 
+		    	if (situacao == 'Reprovado') {
+		    		var labelType = 'label-danger';
+		    		var icon = 'fa-thumbs-o-down';
+		    	}
+	    		$(elem).children().first().addClass(labelType).children().first().addClass(icon);
+    		});
+
 	    }
 	  };
 	});
