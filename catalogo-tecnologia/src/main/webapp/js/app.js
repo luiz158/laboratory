9d8e30046cc694e79be3535046eeeda003bb0064
Robson Ximenes <robsonximenes@gmail.com>
2014-02-10 12:37:44 -0300

    Campos novos analise

--- catalogo-tecnologia/src/main/webapp/js/app.js
+++ catalogo-tecnologia/src/main/webapp/js/app.js
@@ -5,7 +5,8 @@
 angular.module('catalogo', [
   'ngRoute',
   'catalogo.controllers',
-  'catalogo.directives'
+  'catalogo.directives',
+  'mgcrea.ngStrap'
 ]).
 config(['$routeProvider', function($routeProvider) {
 	  $routeProvider.when('/analise', {templateUrl: 'partials/analise-listar.html', controller: 'AnaliseList'});
