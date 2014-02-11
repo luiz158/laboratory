068c1743b1f69e35f1f4a0837f803df073e9c8f3
Robson Ximenes <robsonximenes@gmail.com>
2014-02-11 17:09:05 -0300

    Adicionando o file upload

--- catalogo-tecnologia/src/main/webapp/js/app.js
+++ catalogo-tecnologia/src/main/webapp/js/app.js
@@ -17,32 +17,3 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/analise/edit/:id', {templateUrl: 'partials/analise-edit.html', controller: 'AnaliseEdit'});
   //$routeProvider.otherwise({templateUrl: 'partials/analise-listar.html'});
 }]);
-
-var MyCtrl = [ '$scope', '$upload', function($scope, $upload) {
-	  $scope.onFileSelect = function($files) {
-	    //$files: an array of files selected, each file has name, size, and type.
-	    for (var i = 0; i < $files.length; i++) {
-	      var file = $files[i];
-	      $scope.upload = $upload.upload({
-	        url: 'server/upload/url', //upload.php script, node.js route, or servlet url
-	        // method: POST or PUT,
-	        // headers: {'headerKey': 'headerValue'},
-	        // withCredential: true,
-	        data: {myObj: $scope.myModelObj},
-	        file: file,
-	        // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
-	        /* set file formData name for 'Content-Desposition' header. Default: 'file' */
-	        //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
-	        /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
-	        //formDataAppender: function(formData, key, val){} //#40#issuecomment-28612000
-	      }).progress(function(evt) {
-	        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-	      }).success(function(data, status, headers, config) {
-	        // file is uploaded successfully
-	        console.log(data);
-	      });
-	      //.error(...)
-	      //.then(success, error, progress); 
-	    }
-	  };
-	}];
