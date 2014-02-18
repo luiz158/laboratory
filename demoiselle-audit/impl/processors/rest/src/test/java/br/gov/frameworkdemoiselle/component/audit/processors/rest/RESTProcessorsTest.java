f5c310e4430b242336b0863cafb7b526215649c6
Paulo Gladson Ximenes Pinheiro <paulo.pinheiro@serpro.gov.br>
2014-02-18 14:46:21 -0300

    Criar classes de teste

--- demoiselle-audit/impl/processors/rest/src/test/java/br/gov/frameworkdemoiselle/component/audit/processors/rest/RESTProcessorsTest.java
+++ demoiselle-audit/impl/processors/rest/src/test/java/br/gov/frameworkdemoiselle/component/audit/processors/rest/RESTProcessorsTest.java
@@ -62,15 +62,15 @@ public class RESTProcessorsTest {
      * @throws java.lang.Exception
      */
     @Test
-    public void testCreate() throws Exception {
+    public void testExecute() throws Exception {
 
-    	//TODO implementar testes
+        //TODO implementar testes
         System.out.println("create");
-        instance.execute(new Trail(null, "Teste4Aba", "Usuario1", "id", "Perfil4Aba", "Criou", "Tela inicial", "10.200.255.26", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\": 1, \"nome\": \"Henrique\", \"filho\": \"Rafael\"}", null, null));
-        instance.execute(new Trail(null, "Teste4Aba", "Usuario2", "id", "Perfil4Aba", "Alterou", "Tela inicial", "10.200.255.37", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\": 1, \"nome\": \"Rafael\", \"filho\": \"José\"}", null, null));
-        instance.execute(new Trail(null, "Teste4Aba", "Usuario3", "id", "Perfil4Aba", "Alterou", "Tela inicial", "10.200.255.29", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\": 1, \"nome\": \"José\", \"filho\": \"Henrique\"}", null, null));
-        instance.execute(new Trail(null, "Teste4Aba", "Usuario4", "id", "Perfil4Aba", "Alterou", "Tela inicial", "10.200.255.44", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\": 1, \"nome\": \"Roberto\", \"filho\": \"Silva\"}", null, null));
-        instance.execute(new Trail(null, "Teste4Aba", "Usuario5", "id", "Perfil4Aba", "Excluiu", "Tela inicial", "10.200.255.73", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\": 1, \"nome\": \"Roberto\", \"filho\": \"Silva\"}", null, null));
+        instance.execute(new Trail(null, "Teste4Aba", "Usuario1", "id", "Perfil4Aba", "Criou", "Tela inicial", "10.200.255.26", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\":  \"1\", \"nome\": \"Henrique\", \"filho\": \"Rafael\"}", null, null));
+        instance.execute(new Trail(null, "Teste4Aba", "Usuario2", "id", "Perfil4Aba", "Alterou", "Tela inicial", "10.200.255.37", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\": \"1\", \"nome\": \"Rafael\", \"filho\": \"José\"}", null, null));
+        instance.execute(new Trail(null, "Teste4Aba", "Usuario3", "id", "Perfil4Aba", "Alterou", "Tela inicial", "10.200.255.29", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\": \"1\", \"nome\": \"José\", \"filho\": \"Henrique\"}", null, null));
+        instance.execute(new Trail(null, "Teste4Aba", "Usuario4", "id", "Perfil4Aba", "Alterou", "Tela inicial", "10.200.255.44", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\": \"1\", \"nome\": \"Roberto\", \"filho\": \"Silva\"}", null, null));
+        instance.execute(new Trail(null, "Teste4Aba", "Usuario5", "id", "Perfil4Aba", "Excluiu", "Tela inicial", "10.200.255.73", new Date(), "br.gov.frameworkdemoiselle.serpro.teste4aba", "{\"class\": \"br.gov.serpro.siafi.domain.Trilha\",\"id\": \"1\", \"nome\": \"Roberto\", \"filho\": \"Silva\"}", null, null));
     }
 
 }
