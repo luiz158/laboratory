55357d98e4cfbefd29bcbbbbb1f6f8ad2b375bbf
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2014-02-26 16:56:50 -0300

    Classe Authorizer, que implementa a interface Authorizer do Demoiselle, para iniciar os controles de permissões dos usuários

---
+++ catalogo-tecnologia/src/main/java/br/gov/serpro/catalogo/security/Authorizer.java
@@ -0,0 +1,46 @@
+package br.gov.serpro.catalogo.security;
+
+import javax.inject.Inject;
+
+import br.gov.frameworkdemoiselle.security.SecurityContext;
+import br.gov.serpro.catalogo.entity.Grupo;
+import br.gov.serpro.catalogo.entity.Perfil;
+import br.gov.serpro.catalogo.entity.User;
+import br.gov.serpro.catalogo.persistence.UserDAO;
+
+
+public class Authorizer implements br.gov.frameworkdemoiselle.security.Authorizer{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private SecurityContext securityContext;
+	
+	@Inject
+	private UserDAO userDAO;
+	
+	@Override
+	public boolean hasRole(String role) throws Exception {
+		if(securityContext.isLoggedIn()) {
+			User user = getUser();
+			for(Grupo grupo : user.getGrupos()) {
+				for(Perfil perfil : grupo.getPerfis()) {
+					if(role.equalsIgnoreCase(perfil.toString())) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private User getUser() {
+		return userDAO.loadByCPF(((User) securityContext.getUser()).getName());
+	}
+
+	@Override
+	public boolean hasPermission(String resource, String operation) throws Exception {
+		return false;
+	}
+
+}
