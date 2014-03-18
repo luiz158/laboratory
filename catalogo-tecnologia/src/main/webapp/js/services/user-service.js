b42bc677f48105286e64a10a06072e85bc942e71
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2014-03-18 10:35:17 -0300

    Adição de serviço de busca de usuário no LDAP por CPF

--- catalogo-tecnologia/src/main/webapp/js/services/user-service.js
+++ catalogo-tecnologia/src/main/webapp/js/services/user-service.js
@@ -3,24 +3,41 @@
 /* Services */
 var services = angular.module('catalogo.services');
 
-services.factory('UserService', function($http, $q){
-	
+services.factory('UserService', function($http, $q, AlertService) {
+
 	var grupos = [];
-	var service = {};				
+	var service = {};
 
 	service.getGrupos = function() {
-	    var deferred = $q.defer();
-	    	$http({
-				url : 'api/grupo',
-				method : "GET"
-			}).success(function(data) {
-				grupos = data;	
-				deferred.resolve(grupos);
-			}).error(function(data, status) {	
-				console.log(data, status);
-			});
+		var deferred = $q.defer();
+		$http({
+			url : 'api/grupo',
+			method : "GET"
+		}).success(function(data) {
+			grupos = data;
+			deferred.resolve(grupos);
+		}).error(function(data, status) {
+			console.log(data, status);
+		});
+		return deferred.promise;
+	};
 
-	    return deferred.promise;
+	service.searchByCPF = function(cpf) {
+		var deferred = $q.defer();
+		$http.get('api/user/cpf/' + cpf).success(
+			function(data) {
+				if (data == "") {
+					AlertService.addWithTimeout('warning',
+							'Usuário não cadastrado no LDAP');
+				} else {
+					deferred.resolve(data);
+				}
+			}).error(function(data, status) {
+				if (status == 412) {
+					AlertService.addWithTimeout('danger', data[0].message);
+				}
+		});
+		return deferred.promise;
 	};
 	
 	return service;
