809866036fba1739f013983199a19481d8f75597
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2014-03-18 08:50:48 -0300

    Ajustando o uso do @Path de acordo com a spec jax-rs

---
+++ catalogo-tecnologia/src/main/java/br/gov/serpro/catalogo/rest/RESTApplication.java
@@ -0,0 +1,9 @@
+package br.gov.serpro.catalogo.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RESTApplication extends Application {
+
+}
