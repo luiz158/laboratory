fc75df9e0ef920a35ff6d7777f63c5cf3cbb7121
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2014-03-20 14:46:03 -0300

    Refatoração de alguns serviços e adição de serviço que retorna o usuário logado de forma assíncrona

--- catalogo-tecnologia/src/main/webapp/js/services/auth-service.js
+++ catalogo-tecnologia/src/main/webapp/js/services/auth-service.js
@@ -3,18 +3,38 @@
 /* Services */
 var services = angular.module('catalogo.services');
 
-services.factory('AuthService', function($http, $rootScope) {
+services.factory('AuthService', function($http, $q, $rootScope) {
 
 	var logado = false;
 	$rootScope.usuario = {};
 	
-	function getLoggedUser(){
+	var service = {};
+	
+	service.getLoggedUserService = function(){
+		var deferred = $q.defer();
 		$http({
 			url : "api/auth/user",
 			method : "GET"
 		}).success(function(response) {
-			$rootScope.usuario = response;
+			deferred.resolve(response);
 		}).error(function(response) {
+			console.log("Erro!");
+		});
+		
+		return deferred.promise;
+	};
+	
+	
+	function getLoggedUser(){
+		console.log('getLoggedUser');
+		$http({
+			url : "api/auth/user",
+			method : "GET"
+		}).success(
+			function(response) {
+				$rootScope.usuario = response;
+			}
+		).error(function(response) {
 			$rootScope.usuario = {};
 		});
 	}
@@ -34,46 +54,55 @@ services.factory('AuthService', function($http, $rootScope) {
 		console.log('erro init auth service');
 	});
 
-	return {
-		login : function(credential, callback, errorCallback) {
-			console.log(credential);
-			$http({
-				url : 'api/auth',
-				method : "POST",
-				data : credential,
-				headers : {
-					'Content-Type' : 'application/json;charset=utf8'
-				}
-			}).success(function(response) {
-				$rootScope.usuario = response;
-				logado = true;
-				callback(response);
-				console.log($rootScope.usuario);
-			}).error(function(response, status) {
-				$rootScope.usuario = {};
-				logado = false;
-				errorCallback(response, status);
-			});
-		},
-		logout : function(callback) {
-			console.log('--------LOGOUT----------');
-			$http({
-				url : 'api/auth',
-				method : "DELETE",
-				headers : {
-					'Content-Type' : 'application/json;charset=utf8'
-				}
-			}).success(function(response) {
-				console.log('AuthService Logout Success');
-				logado = false;
-				callback(response.data);
-			});
-		},
-		isLoggedIn : function() {
-			return logado;
-		},
-		getUsuario : function(){
-			return $rootScope.usuario;
-		}
+	
+	service.login = function(credential, callback, errorCallback) {
+		console.log('login: ');
+		console.log(credential);
+		$http({
+			url : 'api/auth',
+			method : "POST",
+			data : credential,
+			headers : {
+				'Content-Type' : 'application/json;charset=utf8'
+			}
+		}).success(function(response) {
+			$rootScope.usuario = response;
+			logado = true;
+			callback(response);
+			console.log($rootScope.usuario);
+		}).error(function(response, status) {
+			$rootScope.usuario = {};
+			logado = false;
+			errorCallback(response, status);
+		});
 	};
+	
+	service.logout = function(callback) {
+		console.log('--------LOGOUT----------');
+		$http({
+			url : 'api/auth',
+			method : "DELETE",
+			headers : {
+				'Content-Type' : 'application/json;charset=utf8'
+			}
+		}).success(function(response) {
+			console.log('AuthService Logout Success');
+			logado = false;
+			callback(response.data);
+		});
+	};
+	
+	service.isLoggedIn = function(){
+		console.log('isLoggedIn: ');
+		console.log(logado);
+		return logado;
+	};
+	
+	service.getUsuario = function(){
+		console.log('getUsuario: ');
+		console.log($rootScope.usuario);
+		return $rootScope.usuario;
+	};
+	
+	return service;
 });
