4c5f8039fe5183c22b493c08b5227bad77b497da
lusabo <lucianosantosborges@gmail.com>
2014-03-20 15:58:26 -0300

    Exclusão de registros no Datatables.

--- incubator/resteasy/example/bookmark/src/main/webapp/js/controller/bookmark-list.js
+++ incubator/resteasy/example/bookmark/src/main/webapp/js/controller/bookmark-list.js
@@ -1,50 +1,70 @@
 $(function() {
-    $("#new").focus();
+	$("#new").focus();
 
-    $(document).ready(function() {
-	findAllOk();
-    });
-
-    $("form").submit(function(event) {
-	event.preventDefault();
-    });
-
-    $("#new").click(function() {
-	location.href = "bookmark-edit.html";
-    });
-
-    $("#delete").click(function() {
-	var ids = [];
-
-	$("input:checked").each(function(index, value) {
-	    ids.push($(value).val());
+	$(document).ready(function() {
+		findAllOk();
 	});
 
-	if (ids.length == 0) {
-	    alert('Nenhum registro selecionado');
-	} else if (confirm('Tem certeza que deseja apagar?')) {
-	    BookmarkProxy.remove(ids, removeOk);
-	}
-    });
+	$("form").submit(function(event) {
+		event.preventDefault();
+	});
+
+	$("#new").click(function() {
+		location.href = "bookmark-edit.html";
+	});
+
+	$("#delete").click(function() {
+		var ids = [];
+
+		$("input:checked").each(function(index, value) {
+			ids.push($(value).val());
+		});
+
+		if (ids.length == 0) {
+			alert('Nenhum registro selecionado');
+		} else if (confirm('Tem certeza que deseja apagar?')) {
+			BookmarkProxy.remove(ids, removeOk);
+		}
+	});
 });
 
 function findAllOk(data) {
-    $('#resultList').dataTable({
-	"aoColumns" : [ {
-	    "mDataProp" : "id"
-	}, {
-	    "mDataProp" : "description"
-	}, {
-	    "mDataProp" : "link"
-	} ],
-	// "aaData" : data,
-	// "bSort" : true,
-	"bServerSide" : true,
-	"sPaginationType" : "full_numbers",
-	"sAjaxSource" : 'api/bookmark/datatables',
-	"bSort" : false
-    });
+	var oTable = $('#resultList').dataTable(
+			{
+				"aoColumns" : [
+						{
+							"aTargets" : [ 0 ],
+							"mData" : "id",
+							"mRender" : function(data) {
+								return '<input id="remove-' + data
+										+ '" type="checkbox" value="' + data
+										+ '">';
+							}
+						}, {
+							"aTargets" : [ 1 ],
+							"mData" : "description"
+						}, {
+							"aTargets" : [ 2 ],
+							"mData" : "link"
+						} ],
+				// "aaData" : data,
+				// "bSort" : true,
+				"oLanguage" : {
+					"sInfo" : "Mostrando _START_ a _END_ de _TOTAL_ registros",
+				},
+				"bRetrieve" : true,
+				//"bDestroy" : true,
+				"bServerSide" : true,
+				"sPaginationType" : "full_numbers",
+				"sAjaxSource" : 'api/bookmark/datatables',
+				"bSort" : false
+			});
+	oTable.fnClearTable();
 }
 
 function removeOk(data) {
+	$.each(data, function(index, value) {
+		// table.removeRow(value);
+		findAllOk();
+	});
 }
