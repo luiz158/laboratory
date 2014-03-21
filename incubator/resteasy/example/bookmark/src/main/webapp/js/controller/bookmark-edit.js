6ca814a65e689e08b6f45f2fe23023b49207d468
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2014-03-21 15:55:16 -0300

    Formatação do código-fonte

--- incubator/resteasy/example/bookmark/src/main/webapp/js/controller/bookmark-edit.js
+++ incubator/resteasy/example/bookmark/src/main/webapp/js/controller/bookmark-edit.js
@@ -1,93 +1,93 @@
 $(function() {
-    $("#delete").hide();
-    $("#description").focus();
+	$("#delete").hide();
+	$("#description").focus();
 
-    $(document).ready(function() {
-	if (id = $.url().param('id')) {
-	    BookmarkProxy.load(id, loadOk, loadFailed);
-	}
-    });
+	$(document).ready(function() {
+		if (id = $.url().param('id')) {
+			BookmarkProxy.load(id, loadOk, loadFailed);
+		}
+	});
 
-    $("form").submit(function(event) {
-	event.preventDefault();
-    });
+	$("form").submit(function(event) {
+		event.preventDefault();
+	});
 
-    $("#save").click(function() {
-	var form = {
-	    description : $("#description").val(),
-	    link : $("#link").val()
-	};
+	$("#save").click(function() {
+		var form = {
+			description : $("#description").val(),
+			link : $("#link").val()
+		};
 
-	if (id = $("#id").val()) {
-	    BookmarkProxy.update(id, form, saveOk, saveFailed);
-	} else {
-	    BookmarkProxy.insert(form, saveOk, saveFailed);
-	}
-    });
+		if (id = $("#id").val()) {
+			BookmarkProxy.update(id, form, saveOk, saveFailed);
+		} else {
+			BookmarkProxy.insert(form, saveOk, saveFailed);
+		}
+	});
 
-    $("#delete").click(function() {
-	if (confirm('Tem certeza que deseja apagar?')) {
-	    BookmarkProxy.remove([ $("#id").val() ], removeOk);
-	}
-    });
+	$("#delete").click(function() {
+		if (confirm('Tem certeza que deseja apagar?')) {
+			BookmarkProxy.remove([ $("#id").val() ], removeOk);
+		}
+	});
 
-    $("#back").click(function() {
-	history.back();
-    });
+	$("#back").click(function() {
+		history.back();
+	});
 });
 
 function loadOk(data) {
-    $("#id-row").show();
-    $("#id-text").html(data.id);
-    $("#id").val(data.id);
-    $("#description").val(data.description);
-    $("#link").val(data.link);
-    $("#delete").show();
+	$("#id-row").show();
+	$("#id-text").html(data.id);
+	$("#id").val(data.id);
+	$("#description").val(data.description);
+	$("#link").val(data.link);
+	$("#delete").show();
 }
 
 function loadFailed(request) {
-    switch (request.status) {
-    case 404:
-	console.log(request);
-	break;
+	switch (request.status) {
+		case 404:
+			console.log(request);
+			break;
 
-    default:
-	break;
-    }
+		default:
+			break;
+	}
 }
 
 function saveOk(data) {
-    history.back();
+	history.back();
 }
 
 function saveFailed(request) {
-    switch (request.status) {
-    case 412:
-	$($("form input").get().reverse()).each(function() {
-	    var id = $(this).attr('id');
-	    var message = null;
+	switch (request.status) {
+		case 412:
+			$($("form input").get().reverse()).each(function() {
+				var id = $(this).attr('id');
+				var message = null;
 
-	    $.each(request.responseJSON, function(index, value) {
-		if (id == value.property) {
-		    message = value.message;
-		    return;
-		}
-	    });
+				$.each(request.responseJSON, function(index, value) {
+					if (id == value.property) {
+						message = value.message;
+						return;
+					}
+				});
 
-	    if (message) {
-		$("#" + id + "-message").html(message).show();
-		$(this).focus();
-	    } else {
-		$("#" + id + "-message").hide();
-	    }
-	});
-	break;
+				if (message) {
+					$("#" + id + "-message").html(message).show();
+					$(this).focus();
+				} else {
+					$("#" + id + "-message").hide();
+				}
+			});
+			break;
 
-    default:
-	break;
-    }
+		default:
+			break;
+	}
 }
 
 function removeOk(data) {
-    history.back();
+	history.back();
 }
--- incubator/resteasy/example/bookmark/src/main/webapp/js/controller/bookmark-list-datatables-server.js
+++ incubator/resteasy/example/bookmark/src/main/webapp/js/controller/bookmark-list-datatables-server.js
@@ -1,74 +1,65 @@
 $(function() {
-    $("#new").focus();
+	$("#new").focus();
 
-    $(document).ready(function() {
-	findAllOk();
-    });
-
-    $("form").submit(function(event) {
-	event.preventDefault();
-    });
-
-    $("#new").click(function() {
-	location.href = "bookmark-edit.html";
-    });
-
-    $("#delete").click(function() {
-	var ids = [];
-
-	$("input:checked").each(function(index, value) {
-	    ids.push($(value).val());
+	$(document).ready(function() {
+		findAllOk();
 	});
 
-	if (ids.length == 0) {
-	    alert('Nenhum registro selecionado');
-	} else if (confirm('Tem certeza que deseja apagar?')) {
-	    BookmarkProxy.remove(ids, removeOk);
-	}
-    });
+	$("form").submit(function(event) {
+		event.preventDefault();
+	});
+
+	$("#new").click(function() {
+		location.href = "bookmark-edit.html";
+	});
+
+	$("#delete").click(function() {
+		var ids = [];
+
+		$("input:checked").each(function(index, value) {
+			ids.push($(value).val());
+		});
+
+		if (ids.length == 0) {
+			alert('Nenhum registro selecionado');
+		} else if (confirm('Tem certeza que deseja apagar?')) {
+			BookmarkProxy.remove(ids, removeOk);
+		}
+	});
 });
 
 function findAllOk(data) {
-    var oTable = $('#resultList').dataTable(
-	    {
-		"aoColumns" : [
-			{
-			    "aTargets" : [ 0 ],
-			    "mData" : "id",
-			    "mRender" : function(id) {
-				return '<input id="remove-' + id
-					+ '" type="checkbox" value="' + id
-					+ '">';
-			    }
-			},
-			{
-			    "aTargets" : [ 1 ],
-			    "mData" : "description",
-			    "mRender" : function(data, type, full) {
-				return '<a href="bookmark-edit.html?id='
-					+ full.id + '">' + full.description
-					+ '</a>';
-			    }
-			},
-			{
-			    "aTargets" : [ 2 ],
-			    "mData" : "link",
-			    "mRender" : function(link) {
-				return '<a href="' + link
-					+ '" target="_blank">' + link + '</a>';
-			    }
-			} ],
+	var oTable = $('#resultList').dataTable({
+		"aoColumns" : [ {
+			"aTargets" : [ 0 ],
+			"mData" : "id",
+			"mRender" : function(id) {
+				return '<input id="remove-' + id + '" type="checkbox" value="' + id + '">';
+			}
+		}, {
+			"aTargets" : [ 1 ],
+			"mData" : "description",
+			"mRender" : function(data, type, full) {
+				return '<a href="bookmark-edit.html?id=' + full.id + '">' + full.description + '</a>';
+			}
+		}, {
+			"aTargets" : [ 2 ],
+			"mData" : "link",
+			"mRender" : function(link) {
+				return '<a href="' + link + '" target="_blank">' + link + '</a>';
+			}
+		} ],
 		"oLanguage" : {
-		    "sInfo" : "Mostrando _START_ a _END_ de _TOTAL_ registros",
-		    "sEmptyTable" : "Não há dados disponíveis na tabela",
-		    "sLengthMenu" : "Mostrar _MENU_ registros",
-		    "sInfoThousands" : ".",
-		    "oPaginate" : {
-			"sFirst" : "Primeiro",
-			"sLast" : "Último",
-			"sNext" : "Próximo",
-			"sPrevious" : "Anterior"
-		    }
+			"sInfo" : "Mostrando _START_ a _END_ de _TOTAL_ registros",
+			"sEmptyTable" : "Não há dados disponíveis na tabela",
+			"sLengthMenu" : "Mostrar _MENU_ registros",
+			"sInfoThousands" : "",
+			"oPaginate" : {
+				"sFirst" : "Primeiro",
+				"sLast" : "Último",
+				"sNext" : "Próximo",
+				"sPrevious" : "Anterior"
+			}
 		},
 		"bFilter" : false,
 		"bRetrieve" : true,
@@ -76,10 +67,10 @@ function findAllOk(data) {
 		"sAjaxSource" : 'api/bookmark/datatables',
 		"bServerSide" : true,
 		"bSort" : false
-	    });
-    oTable.fnClearTable();
+	});
+	oTable.fnClearTable();
 }
 
 function removeOk(data) {
-    findAllOk();
+	findAllOk();
 }
