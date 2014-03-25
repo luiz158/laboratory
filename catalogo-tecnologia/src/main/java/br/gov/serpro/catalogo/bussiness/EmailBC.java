ecb86eba72e272977522fa33c3b588ebc7aa0d58
Robson Ximenes <robsonximenes@gmail.com>
2014-03-25 11:38:01 -0300

    Comentando o envio de e-mails enquanto não configuramos o servidor

--- catalogo-tecnologia/src/main/java/br/gov/serpro/catalogo/bussiness/EmailBC.java
+++ catalogo-tecnologia/src/main/java/br/gov/serpro/catalogo/bussiness/EmailBC.java
@@ -15,18 +15,18 @@ import br.gov.serpro.catalogo.entity.User;
 @BusinessController
 public class EmailBC {
 
-	@Inject 
-	private Mail mail;
+	//@Inject 
+	//private Mail mail;
 
 	public void enviarEmailUsuarioAdicionadoEquipe(User usuario, Fase fase) {
 		if(usuario.getEmail()!=null && !usuario.getEmail().isEmpty()){	
 			Beans.getReference(Logger.class).debug(usuario.getEmail());
-			mail
-			.to(usuario.getEmail())
-			.from("catalogo-tecnologia@serpro.gov.br")
-			.body().html("<strong>"+usuario.getName()+"</strong>, você foi adicionado a equipe de <a href='http://localhost:8080/catalogo-tecnologia/#"+fase.getFase().name().toLowerCase()+"/edit/"+fase.getId()+"'>"+fase.getFase().name()+" </a>" )
-			.subject("Assunto")
-			.send();
+//			mail
+//			.to(usuario.getEmail())
+//			.from("catalogo-tecnologia@serpro.gov.br")
+//			.body().html("<strong>"+usuario.getName()+"</strong>, você foi adicionado a equipe de <a href='http://localhost:8080/catalogo-tecnologia/#"+fase.getFase().name().toLowerCase()+"/edit/"+fase.getId()+"'>"+fase.getFase().name()+" </a>" )
+//			.subject("Assunto")
+//			.send();
 		}else{
 			throw new ValidationException().addViolation(null, "Usuário ("+usuario.getCPF()+") não possui e-mail");
 		}
