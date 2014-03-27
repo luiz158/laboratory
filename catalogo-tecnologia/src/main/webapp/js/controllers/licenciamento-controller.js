b31d1eff68c66ce0f25f80736373c31a11bd5ab4
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2014-03-27 14:43:14 -0300

    Adição de mensagem adequada ao usuário no caso de falha de autorização para adicionar, alterar ou excluir licenciamento

--- catalogo-tecnologia/src/main/webapp/js/controllers/licenciamento-controller.js
+++ catalogo-tecnologia/src/main/webapp/js/controllers/licenciamento-controller.js
@@ -24,13 +24,14 @@ controllers.controller('LicenciamentoList',
 				$http({
 					url : 'api/licenciamento/' + id,
 					method : "DELETE"
-
 				}).success(function(data) {
 					carregarLicenciamentos();
-
 				}).error(function(data, status) {
-					AlertService.addWithTimeout('danger','Não foi possível executar a operação');
-					console.log('vai vltar...');
+					if (status == 401) {
+						AlertService.addWithTimeout('warning', data.message);
+					} else {
+						AlertService.addWithTimeout('danger','Não foi possível executar a operação');
+					}
 				});
 			};
 
@@ -65,18 +66,19 @@ controllers.controller('LicenciamentoEdit', function Licenciamento($scope, $http
 			headers : {
 				'Content-Type' : 'application/json;charset=utf8'
 			}
-	
 		}).success(function(data) {
 			AlertService.addWithTimeout('success','Licenciamento salvo com sucesso');
 			$location.path('licenciamento');
-		}).error(
-				function(data, status) {
-					if (status = 412) {
-						$.each(data, function(i, violation) {
-							$("#" + violation.property + "-message").text(
-									violation.message);
-						});
-					}
+		}).error(function(data, status) {
+			if (status = 412) {
+				$.each(data, function(i, violation) {
+					$("#" + violation.property + "-message").text(violation.message);
+				});
+			} else if (status == 401){
+				AlertService.addWithTimeout('warning', data.message);
+			} else {
+				AlertService.addWithTimeout('danger','Não foi possível executar a operação');
+			}
 		});
 	};
 });
