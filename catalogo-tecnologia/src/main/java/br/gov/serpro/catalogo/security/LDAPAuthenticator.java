adcbcdf7d3168ea56ca5bae3da559fdcec95467f
Robson Ximenes <robsonximenes@gmail.com>
2014-04-08 11:09:40 -0300

    0000017 e 18: Desabilitar ou habilitar os campos da tela de internalização e declinio

--- catalogo-tecnologia/src/main/java/br/gov/serpro/catalogo/security/LDAPAuthenticator.java
+++ catalogo-tecnologia/src/main/java/br/gov/serpro/catalogo/security/LDAPAuthenticator.java
@@ -18,10 +18,10 @@ import javax.servlet.http.HttpSession;
 
 import br.gov.frameworkdemoiselle.security.Authenticator;
 import br.gov.frameworkdemoiselle.security.Credentials;
+import br.gov.frameworkdemoiselle.security.InvalidCredentialsException;
 import br.gov.frameworkdemoiselle.util.Beans;
 import br.gov.serpro.catalogo.bussiness.UsuarioBC;
 import br.gov.serpro.catalogo.entity.User;
-import br.gov.serpro.catalogo.persistence.UserDAO;
 
 @SessionScoped
 public class LDAPAuthenticator implements Authenticator {
@@ -32,69 +32,74 @@ public class LDAPAuthenticator implements Authenticator {
 	private Credentials credentials;
 
 	private User user;
-	
+
 	@Inject
 	private LDAPConfig ldapConfig;
-		
-	@Inject 
+
+	@Inject
 	private UsuarioBC usuarioBC;
 
 	@Override
 	public void authenticate() throws Exception {
-		
+
 		// Criando o usuário admin, caso não exista.
-		if(credentials.getUsername().equals("admin")){
-			if(credentials.getPassword().equals("adminCetec")){
-				user = usuarioBC.iniciarUsuario();				
+		if (credentials.getUsername().equals("admin")) {
+			if (credentials.getPassword().equals("adminCetec")) {
+				user = usuarioBC.iniciarUsuario();
+				return;
+			}else{
+				throw new InvalidCredentialsException("usuário ou senha inválidos");
 			}
-			return;
 		}
-		
-			SearchControls controls = createSearchControls();
-			String filter = createCPFFilter(credentials.getUsername());
-			SearchResult searchResult = createSearchResult(controls, filter);
 
-			LdapContext ldapContext = createContext(searchResult.getNameInNamespace(), credentials.getPassword());
-			ldapContext.close();
+		SearchControls controls = createSearchControls();
+		String filter = createCPFFilter(credentials.getUsername());
+		SearchResult searchResult = createSearchResult(controls, filter);
 
-			user = usuarioBC.carregarOuInserir(createUser(searchResult.getAttributes()));			
+		LdapContext ldapContext = createContext(
+				searchResult.getNameInNamespace(), credentials.getPassword());
+		ldapContext.close();
 
-			List<String> roles = new ArrayList<String>();
-			roles.add("logado");			
-			user.setAttribute("roles", roles);
-			
-			
-		/*} catch (Exception cause) {
-			throw new InvalidCredentialsException("usuário ou senha inválidos");
-		} catch (AuthenticationException cause) {
-			
-		}*/
+		user = usuarioBC.carregarOuInserir(createUser(searchResult
+				.getAttributes()));
+
+		List<String> roles = new ArrayList<String>();
+		roles.add("logado");
+		user.setAttribute("roles", roles);
+
+		/*
+		 * } catch (Exception cause) { throw new
+		 * InvalidCredentialsException("usuário ou senha inválidos"); } catch
+		 * (AuthenticationException cause) {
+		 * 
+		 * }
+		 */
 	}
-	
-	
-	
-	
+
 	public User searchUserByCPF(String cpf) throws NamingException {
 		SearchControls controls = createSearchControls();
 		String filter = createCPFFilter(cpf);
 		SearchResult searchResult = createSearchResult(controls, filter);
-		if(searchResult == null){
+		if (searchResult == null) {
 			return null;
 		}
 		return createUser(searchResult.getAttributes());
 	}
-	
-	public List<User> searchUserByDisplayName(String displayName) throws NamingException {
+
+	public List<User> searchUserByDisplayName(String displayName)
+			throws NamingException {
 		SearchControls controls = createSearchControls();
 		String filter = createDisplayNameFilter(displayName);
-		NamingEnumeration<SearchResult> searchResults = createListSearchResult(controls, filter);
-		if(searchResults == null){
+		NamingEnumeration<SearchResult> searchResults = createListSearchResult(
+				controls, filter);
+		if (searchResults == null) {
 			return null;
 		}
-		
+
 		List<User> users = new ArrayList<User>();
-		while(searchResults.hasMore()) {
-			users.add(createUser(((SearchResult)searchResults.next()).getAttributes()));
+		while (searchResults.hasMore()) {
+			users.add(createUser(((SearchResult) searchResults.next())
+					.getAttributes()));
 		}
 		return users;
 	}
@@ -102,7 +107,7 @@ public class LDAPAuthenticator implements Authenticator {
 	@Override
 	public void unauthenticate() throws Exception {
 		user = null;
-		Beans.getReference(HttpSession.class).invalidate();		
+		Beans.getReference(HttpSession.class).invalidate();
 	}
 
 	@Override
@@ -115,25 +120,26 @@ public class LDAPAuthenticator implements Authenticator {
 
 		result.setCPF(attributes.get("uid").get().toString());
 		result.setName(attributes.get("cn").get().toString());
-		if(attributes.get("mail") == null) {
+		if (attributes.get("mail") == null) {
 			result.setEmail("");
-		}else {
+		} else {
 			result.setEmail(attributes.get("mail").get().toString());
 		}
-		if(attributes.get("telephoneNumber") == null) {
+		if (attributes.get("telephoneNumber") == null) {
 			result.setTelephoneNumber("");
-		}else {
-			result.setTelephoneNumber(attributes.get("telephoneNumber").get().toString());
+		} else {
+			result.setTelephoneNumber(attributes.get("telephoneNumber").get()
+					.toString());
 		}
-		if(attributes.get("ou") == null) {
+		if (attributes.get("ou") == null) {
 			result.setSetor("");
-		}else {
+		} else {
 			result.setSetor(attributes.get("ou").get().toString());
 		}
-		
+
 		return result;
 	}
-	
+
 	private SearchControls createSearchControls() {
 		final SearchControls result = new SearchControls();
 		result.setSearchScope(ldapConfig.getSearchScope());
@@ -147,40 +153,48 @@ public class LDAPAuthenticator implements Authenticator {
 
 		return result;
 	}
-	
+
 	private String createDisplayNameFilter(String displayname) {
 		String result = ldapConfig.getNameFilter();
 		result = result.replaceAll("\\{0\\}", displayname);
 
 		return result;
 	}
-	
-	private SearchResult createSearchResult(SearchControls controls, String filter) throws NamingException {
+
+	private SearchResult createSearchResult(SearchControls controls,
+			String filter) throws NamingException {
 		LdapContext ldapContext = createContext();
-		final NamingEnumeration<SearchResult> naming = ldapContext.search(ldapConfig.getBaseCtxDN(), filter, controls);
+		final NamingEnumeration<SearchResult> naming = ldapContext.search(
+				ldapConfig.getBaseCtxDN(), filter, controls);
 		ldapContext.close();
 
 		return naming.hasMoreElements() ? naming.next() : null;
 	}
-	
-	private NamingEnumeration<SearchResult> createListSearchResult(SearchControls controls, String filter) throws NamingException {
+
+	private NamingEnumeration<SearchResult> createListSearchResult(
+			SearchControls controls, String filter) throws NamingException {
 		LdapContext ldapContext = createContext();
-		final NamingEnumeration<SearchResult> naming = ldapContext.search(ldapConfig.getBaseCtxDN(), filter, controls);
+		final NamingEnumeration<SearchResult> naming = ldapContext.search(
+				ldapConfig.getBaseCtxDN(), filter, controls);
 		ldapContext.close();
 
 		return naming.hasMoreElements() ? naming : null;
 	}
-	
+
 	private LdapContext createContext() throws NamingException {
-		return createContext(ldapConfig.getBindDN(), ldapConfig.getBindCredential());
+		return createContext(ldapConfig.getBindDN(),
+				ldapConfig.getBindCredential());
 	}
-	
-	private LdapContext createContext(String username, String password) throws NamingException {
+
+	private LdapContext createContext(String username, String password)
+			throws NamingException {
 		final Hashtable<String, String> env = new Hashtable<String, String>();
 
-		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+		env.put(Context.INITIAL_CONTEXT_FACTORY,
+				"com.sun.jndi.ldap.LdapCtxFactory");
 		env.put(Context.PROVIDER_URL, ldapConfig.getProviderURL());
-		env.put(Context.SECURITY_AUTHENTICATION, ldapConfig.getSecurityAuthentication());
+		env.put(Context.SECURITY_AUTHENTICATION,
+				ldapConfig.getSecurityAuthentication());
 		env.put(Context.SECURITY_PRINCIPAL, username);
 		env.put(Context.SECURITY_CREDENTIALS, password);
 		env.put(Context.REFERRAL, "follow");
@@ -188,5 +202,4 @@ public class LDAPAuthenticator implements Authenticator {
 		return new InitialLdapContext(env, null);
 	}
 
-	
 }
