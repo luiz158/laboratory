c8de7502697ba4c520490b5968cf2b3a23dc5ada
Robson Ximenes <robsonximenes@gmail.com>
2014-04-30 10:26:13 -0300

    Grafico de produtos

--- catalogo-tecnologia/src/main/webapp/js/controllers/dashboard.js
+++ catalogo-tecnologia/src/main/webapp/js/controllers/dashboard.js
@@ -3,7 +3,7 @@
 /* Controllers */
 var controllers = angular.module('catalogo.controllers');
 
-controllers.controller('DashboardCtrl', function DashboardCtrl($scope, DashboardService) {
+controllers.controller('DashboardCtrl', function DashboardCtrl($scope, DashboardService, $filter) {
 	
 	/* Paginação */
 	$scope.paginacaoDemandas = {
@@ -71,5 +71,75 @@ controllers.controller('DashboardCtrl', function DashboardCtrl($scope, Dashboard
 	    	]
 	    }
 	];
+	
+	
+	
+	var templateMercado = '<img src="images/Rocket.png"> {version} ';
+	var templateSerpro = '<img src="images/serpro.png"> 1.5 - {fases}';
+	
+	function getFasesUrl(f){
+		return '<a href="#'+$filter("faseUrl")(f)+'/'+f.id+'"></a>';
+	}
+	
+	$scope.dados  = [
+        {
+            'start': new Date(2010,2,23),
+            'content': templateMercado.replace("{version}", "1.4"),
+            'className': 'version1'
+            	
+        },
+        {
+            'start': new Date(2011,2,23),
+            'content': templateMercado.replace("{version}", "1.5"),
+            'className': 'version2'
+        },
+        {
+            'start': new Date(2012,2,23),
+            'content': templateMercado.replace("{version}", "1.6"),
+            'className': 'version3'
+        },
+        {
+            'start': new Date(2013,2,23),
+            'content': templateMercado.replace("{version}", "1.7"),
+            'className': 'version4'
+        },
+        {
+            'start': new Date(2014,2,23),
+            'content': templateMercado.replace("{version}", "1.8"),
+            'className': 'version5'
+        },
+        {
+            'start': new Date(2010,7,23),
+            'end': new Date(2011,6,23),
+            'content': '1.4 - Prospecção',
+            'className': 'version1'
+        },
+        {
+            'start': new Date(2011,6,24),
+            'end': new Date(2011,12,23),
+            'content': '1.4 - Internalização',
+            'className': 'version1'
+        },
+        {
+            'start': new Date(2011,12,24),
+            'end': new Date(2013,12,23),
+            'content': '1.4 - Sustentação',
+            'className': 'version1'
+        },
+        {
+            'start': new Date(2012,7,23),
+            'end': new Date(2014,12,23),
+            'content': templateSerpro.replace("{version}", "1.8").replace("{fases}",getFasesUrl({id: 32, fase: 'INTERNALIZACAO'}) ),
+            'className': 'version2'
+        },
+        {
+            'start': new Date(2014,1,1),
+            'end': new Date(2014,4,23),
+            'content': '1.6 - Prospecção',
+            'className': 'version3'
+        },
+    ];
+	
+	
 
 });
